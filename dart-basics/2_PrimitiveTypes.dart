6cddcbad90d1519155c0408b36eff85e89f2d57b
sagar <55848762+TekCreek@users.noreply.github.com>
2025-07-02 11:58:33 +0530

    Added primitive types and records.

---
+++ dart-basics/2_PrimitiveTypes.dart
@@ -0,0 +1,63 @@
+/**
+ * Numbers (int, double) - num can be either int or double
+ * String
+ * bool 
+ * Records  - (value1, value2)
+ * Function
+ * List 
+ * Set
+ * Map
+ * 
+ * const - constants can not be modified after initialization
+ * 
+ */
+void main() {
+  int a = 1;
+  print(a);
+
+  double b = 1.2;
+  print(b);
+
+  num c = 1; // x can have both int and double values
+  c += 2.5;
+  print(c);
+
+  var d = 10; // type inferred using value
+  print(d);
+
+  var one = int.parse('1');
+  print(one);
+
+  String oneAsString = 1.toString();
+  print(oneAsString);
+
+  const msPerSecond = 1000;
+  // msPerSecond++; // ERROR
+  print(msPerSecond);
+
+  String s = 'String interpolation';
+  print("value of s is $s");
+
+  // using triple single or double quotes
+  var multiLineStr = '''
+You can create
+multi-line strings like this one.
+''';
+
+  print(multiLineStr);
+
+  var rawString = r'In a raw string, not even \n gets special treatment.';
+  print(rawString);
+
+  // constant string can only interpolate constants.
+  var myVar = 10;
+  // const STR = 'The value of myVar is $myVar'; // ERROR: since myVar is not constant
+  // print(STR);
+
+  const PI = 3.14;
+  const STR1 = 'The value of PI is $PI'; // WORKS
+  print(STR1);
+
+  bool flag = true; // can be either true of false
+  print(flag);
+}
