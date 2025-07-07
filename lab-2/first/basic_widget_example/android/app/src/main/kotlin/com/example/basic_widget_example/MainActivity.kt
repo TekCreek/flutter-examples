1235fb8281fa7d2d5af0dfb96d63f40a60f0f197
sagar <55848762+TekCreek@users.noreply.github.com>
2025-07-07 13:47:08 +0530

    Lab2 example1 added

---
+++ lab-2/first/basic_widget_example/android/app/src/main/kotlin/com/example/basic_widget_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.basic_widget_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lab-2/first/basic_widget_example/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed:
+            Colors.blue, // Changed color scheme for Flutter examples
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Widget demo')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Hello, Flutter!', style: TextStyle(fontSize: 18.0)),
+            const Text(
+              'Styled Text',
+              style: TextStyle(
+                color: Colors.deepPurple,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            Image.network(
+              'https://www.gstatic.com/flutter-onestack-prototype/genui/example_1.jpg',
+              width: 150,
+              height: 150,
+              fit: BoxFit.cover,
+            ),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(20.0),
+              color: Colors.lightBlue[100],
+              child: const Text('Padded & Margined container'),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
