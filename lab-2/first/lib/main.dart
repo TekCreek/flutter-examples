efcc32a7d443c832f514d3fe28ba335972ede33d
Sagar Uppuluri <55848762+TekCreek@users.noreply.github.com>
2025-07-12 12:05:16 +0530

    Changed lab1 example

--- lab-2/first/lib/main.dart
+++ lab-2/first/lib/main.dart
@@ -3,41 +3,32 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed:
-            Colors.blue, // Changed color scheme for Flutter examples
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Widget demo')),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Hello, Flutter!'),
-            const Text(
-              'Styled Text',
-              style: TextStyle(
-                color: Colors.deepPurple,
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-              ),
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Hello, Flutter!'),
+          const Text(
+            'Styled Text',
+            style: TextStyle(
+              color: Colors.deepPurple,
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
             ),
-            Image.network(
-              'https://www.gstatic.com/flutter-onestack-prototype/genui/example_1.jpg',
-              width: 150,
-              height: 150,
-              fit: BoxFit.cover,
-            ),
-            Container(
-              margin: const EdgeInsets.all(10.0),
-              padding: const EdgeInsets.all(20.0),
-              color: Colors.lightBlue[100],
-              child: const Text('Padded & Margined container'),
-            ),
-          ],
-        ),
+          ),
+          Image.network(
+            'https://www.gstatic.com/flutter-onestack-prototype/genui/example_1.jpg',
+            width: 150,
+            height: 150,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            margin: const EdgeInsets.all(40.0),
+            padding: const EdgeInsets.all(40.0),
+            color: Colors.lightBlue[100],
+            child: const Text('Padded & Margined container'),
+          ),
+        ],
       ),
     ),
   );
