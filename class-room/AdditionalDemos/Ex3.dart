589d37d5fa85a86d8a21209dc3c6702d0759f51c
sagar <55848762+TekCreek@users.noreply.github.com>
2025-07-19 15:03:45 +0530

    changed variable name

--- class-room/AdditionalDemos/Ex3.dart
+++ class-room/AdditionalDemos/Ex3.dart
@@ -1,18 +1,27 @@
+// A simple and easy example to understand async await.
+
+// Note - Not 100% accurate example
+
 void main() async {
   // mother
-  Future<String> product = buy('paneer');
-  product.then((p) {
+  Future<String> paneerReceived = buy('paneer');
+
+  paneerReceived.then((p) {
     print('prepare biryani with the $p');
   });
+
   print('ready rice');
   print('cut onions etc');
 }
 
 // you
 Future<String> buy(String productName) async {
+  // Simulate the delay
   await Future.delayed(Duration(milliseconds: 100));
+
   print('walk to the store');
   print('order the product');
   print('pay the amount');
+
   return "[$productName]";
 }
