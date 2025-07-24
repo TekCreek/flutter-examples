ab8c2e68264eb49db05ab35df89fedea4dcef71c
sagar <55848762+TekCreek@users.noreply.github.com>
2025-07-24 11:27:11 +0530

    Lab 1.1. added

--- class-room/AdditionalDemos/Ex6.dart
+++ class-room/AdditionalDemos/Ex6.dart
@@ -1,7 +1,17 @@
 void main() {
-  List<int> list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  List<int> newList = list.where((x) => x % 2 == 0)
-                          .map((x) => x * 2)
-                          .toList();
-  print(newList);
+  List<int> data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // List<int> newCollection = [];
+  // for (var ele in data) {
+  //   if (ele % 2 == 0) {
+  //     newCollection.add(ele * ele);
+  //   }
+  // }
+  // print(newCollection);
+
+  List<int> newCollection = data
+      .where((x) => x % 2 == 0)
+      .map((x) => x * x)
+      .toList();
+
+  print(newCollection);
 }
---
+++ lab-1.1/Readme.md
@@ -0,0 +1,5 @@
+# 1.1 - Additional lab 
+
+a) Write a sort function to sort the list of integers using any sorting technique, 
+this function should accept comparator function passed as argument and use it for comparison
+logic. Demonstrate the usage of the sort function through sample data. 
